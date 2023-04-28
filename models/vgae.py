299dadff08c11ab689b60d57c5bec3de2e3bf85a
TeoReu <reu.teodora@gmail.com>
2023-04-28 18:12:19 +0100

    first commit

--- models/vgae.py
+++ models/vgae.py
@@ -77,7 +77,7 @@ class L1VGAE(VGAE):
         I.to(self.device)
         a.to(self.device)
 
-        lap_sym = I - torch.mm(torch.mm(D, a), D)
+        lap_sym = I - torch.mm(torch.mm(D, a).to(self.device), D)
 
         eigenvalues = torch.sort(torch.real(torch.linalg.eigvals(lap_sym)))
 
