0a67bdde085d6d91b0e59b547226404410be5522
TeoReu <reu.teodora@gmail.com>
2023-04-29 14:17:21 +0100

    first commit

--- models/special_supervised.py
+++ models/special_supervised.py
@@ -1,6 +1,7 @@
 from torch import nn
 from torch.nn import Embedding, Linear
 from torch_geometric.nn import GCN, VGAE
+from torch_geometric.utils import dense_to_sparse
 from torch_scatter import scatter_sum
 
 from models.vgae import VariationalEncoder
@@ -8,12 +9,11 @@ from models.vgae import VariationalEncoder
 
 class VGAEGCN(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim, num_layers=2,
-                 molecular=True, trans=True, vae=True):
-        super(GCN, self).__init__()
+                 molecular=True, trans=False):
+        super(VGAEGCN, self).__init__()
         self.num_layers = num_layers  # please select num_layers>=2
         self.molecular = molecular
         self.trans = trans
-        self.vae = vae
 
         if self.molecular:
             self.embed_x = Embedding(28, hidden_dim)
@@ -22,26 +22,19 @@ class VGAEGCN(nn.Module):
 
         self.vgae = VGAE(VariationalEncoder(in_channels=1, out_channels=hidden_dim, layers=2, molecular=True, transform=False))
 
-        if self.trans:
-            self.lin_trans = Linear(5, hidden_dim)
-
         self.layers = GCN(in_channels=hidden_dim, hidden_channels=hidden_dim, num_layers=num_layers, out_channels=output_dim)
 
 
     def forward(self, data):
+        x_1 = self.embed_x(data.x.long()).squeeze(1)
         z = self.vgae.encode(data)
 
         gen_adj = self.vgae.decoder.forward_all(z)
 
-        new_edge_index = to_
+        new_edge_index, new_edge_attr = dense_to_sparse(gen_adj)
 
-
-        x_1 = self.layers(x, data.edge_index)
-
-        if self.vae:
-            x_vae = self.vae_layers(x, data.vr_edge_index)
-            x_1 = x_vae + x_1
+        x_1 = self.layers(x_1, new_edge_index)
 
         y_hat = scatter_sum(x_1, data.batch, dim=0)
         y_hat = y_hat.squeeze(-1)
-        return y_hat, x
+        return y_hat, data.x
