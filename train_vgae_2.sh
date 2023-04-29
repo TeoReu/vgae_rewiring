5df9edf59928c3fff19f70f42c91d8e3dcca5b6c
TeoReu <reu.teodora@gmail.com>
2023-04-29 09:01:36 +0100

    first commit

---
+++ train_vgae_2.sh
@@ -0,0 +1,16 @@
+for model in "GCN"
+do
+  for split_graph in "_"
+  do
+    for layers in 1 2 4 8
+    do
+      for alpha in  -10 -5 0 0.1 0.5 1
+      do
+        for transform in  False
+        do
+          python zinc_vgae.py --model $model --split_graph $split_graph  --layers $layers --file_name "results" --alpha $alpha --transform $transform
+        done
+      done
+    done
+  done
+done
