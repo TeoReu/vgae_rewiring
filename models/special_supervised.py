119c0d6713cb2d2dda3ff69cd4bebc52bca0d090
TeoReu <reu.teodora@gmail.com>
2023-04-29 16:45:20 +0100

    first commit

--- models/special_supervised.py
+++ models/special_supervised.py
@@ -43,3 +43,31 @@ class VGAEGCN(nn.Module):
         y_hat = scatter_sum(x, data.batch, dim=0)
         y_hat = y_hat.squeeze(-1)
         return y_hat, data.x
+
+
+class NormalGCN(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim, num_layers=2,
+                 molecular=True, trans=False):
+        super(NormalGCN, self).__init__()
+        self.num_layers = num_layers  # please select num_layers>=2
+        self.molecular = molecular
+        self.trans = trans
+
+        if self.molecular:
+            self.embed_x = Embedding(28, hidden_dim)
+        else:
+            self.embed_x = Linear(input_dim, hidden_dim)
+
+        self.vgae = VGAE(VariationalEncoder(in_channels=1, out_channels=hidden_dim, layers=2, molecular=True, transform=False))
+
+        self.vgae_layers = GCN(in_channels=hidden_dim, hidden_channels=hidden_dim, num_layers=num_layers, out_channels=output_dim)
+        self.layers= GCN(in_channels=hidden_dim, hidden_channels=hidden_dim, num_layers=num_layers, out_channels=output_dim)
+
+    def forward(self, data):
+        x = self.embed_x(data.x.long()).squeeze(1)
+
+        x = self.layers(x, data.edge_index)
+
+        y_hat = scatter_sum(x, data.batch, dim=0)
+        y_hat = y_hat.squeeze(-1)
+        return y_hat, data.x
