6daa3b6a640185e688a40f9b6b47a1c746027ed1
TeoReu <reu.teodora@gmail.com>
2023-05-01 22:25:51 +0100

    first commit

--- zinc_classifier.sh
+++ zinc_classifier.sh
@@ -2,13 +2,13 @@ for model in "vr"
 do
   for layers in 4
   do
-    for vae_layers in 2 4
+    for vae_layers in 1 2 4 8
     do
-      for alpha in 0.1
+      for alpha in 0.0 0.1 0.5 1.0
       do
-          for threshold in 0.65
+          for threshold in 0.65 0.8 0.95
           do
-            for transform in 0 1
+            for transform in 1
             do
               for nr in 123 234 345
               do
@@ -21,16 +21,3 @@ do
   done
 done
 
-for model in "simple"
-do
-  for layers in 4
-  do
-    for transform in 0 1
-    do
-      for nr in 123 234 345
-      do
-        python zinc_classifier.py --model $model --layers $layers --file_name "results_regression_zinc" --transform $transform --nr $nr
-      done
-    done
-  done
-done
