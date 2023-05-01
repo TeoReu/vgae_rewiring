818aad2fcd53e687845c1e8bf1baa5b3aa08dfc8
TeoReu <reu.teodora@gmail.com>
2023-05-01 18:57:52 +0100

    first commit

--- models/special_supervised.py
+++ models/special_supervised.py
@@ -45,6 +45,44 @@ class VGAEGCN(nn.Module):
         return y_hat, data.x
 
 
+class VGAEGCN_init_vae(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim, vae, num_layers=2,
+                 molecular=True, trans=False):
+        super(VGAEGCN_init_vae, self).__init__()
+        self.num_layers = num_layers  # please select num_layers>=2
+        self.molecular = molecular
+        self.trans = trans
+
+        if self.molecular:
+            self.embed_x = Embedding(28, hidden_dim)
+        else:
+            self.embed_x = Linear(input_dim, hidden_dim)
+
+        self.vgae = vae
+
+        self.vgae_layers = GCN(in_channels=hidden_dim, hidden_channels=hidden_dim, num_layers=num_layers, out_channels=output_dim)
+        self.layers= GCN(in_channels=hidden_dim, hidden_channels=hidden_dim, num_layers=num_layers, out_channels=output_dim)
+
+    def forward(self, data):
+        x = self.embed_x(data.x.long()).squeeze(1)
+
+
+        z = self.vgae.encode(data)
+
+        gen_adj = self.vgae.decoder.forward_all(z)
+        gen_adj = ReLU()(gen_adj - 0.65)
+
+        new_edge_index, new_edge_weight = dense_to_sparse(gen_adj)
+
+        x_0 = self.layers(x, data.edge_index)
+        x_1 = self.vgae_layers(x, new_edge_index, edge_weight=new_edge_weight)
+
+        x = x_0 + x_1
+        y_hat = scatter_sum(x, data.batch, dim=0)
+        y_hat = y_hat.squeeze(-1)
+        return y_hat, data.x
+
+
 class NormalGCN(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim, num_layers=2,
                  molecular=True, trans=False):
---
+++ zinc_special_classifier.sh
@@ -0,0 +1,22 @@
+for model in "vr"
+do
+  for layers in 4
+  do
+    for vae_layers in 2 4
+    do
+      for alpha in 0.1
+      do
+          for threshold in 0.65
+          do
+            for transform in 0 1
+            do
+              for nr in 123 234 345
+              do
+                python zinc_classifier.py --model $model --vae_layers $vae_layers --threshold $threshold --layers $layers --alpha $alpha --file_name "special_results_regression_zinc" --transform $transform --nr $nr
+              done
+            done
+          done
+      done
+    done
+  done
+done
