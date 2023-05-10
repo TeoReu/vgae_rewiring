3168a86da286a2508fd268d6aed0e4ead5edc7dd
TeoReu <reu.teodora@gmail.com>
2023-05-10 02:46:44 +0100

    first commit

--- zinc_simple_vgae.sh
+++ zinc_simple_vgae.sh
@@ -4,7 +4,7 @@ do
   do
     for layers in 4 6
     do
-      for alpha in -10 -5 -1 0 0.01 0.1 1 5
+      for alpha in   0 -1 0.01 -5 0.1 -10 1 5
       do
         python zinc_simple_vgae.py --model $model --layers $layers --file_name "peptides_simple_vgae_study_over_alpha" --transform $transform --alpha $alpha
       done
